4af7f846b058bbb42b061759f89d7c5398e0b704
vionaosiako <viona.osiako@student.morigaschool.com>
2022-05-13 15:08:23 +0300

    Import db and models in manager.py

--- manage.py
+++ manage.py
@@ -1,7 +1,8 @@
 from flask import Flask
-from app import create_app
+from app.models import *
+from app import create_app,db
 from flask_script import Manager,Server
-# from  flask_migrate import Migrate, MigrateCommand
+from  flask_migrate import Migrate, MigrateCommand
 
 # from app.models import User
 
@@ -13,8 +14,8 @@ app = create_app('development')
 manager = Manager(app)
 manager.add_command('server',Server)
 
-# migrate = Migrate(app,db)
-# manager.add_command('db',MigrateCommand)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 
 # @manager.command
