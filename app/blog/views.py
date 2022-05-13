9275cf6d44135580a86812b24f06f4c53f9bdeef
vionaosiako <viona.osiako@student.morigaschool.com>
2022-05-13 19:54:31 +0300

    Updates the views.py

---
+++ app/blog/views.py
@@ -0,0 +1,11 @@
+from . . import db
+from . forms import BlogForm
+from . import Blog
+from flask import render-template, redirect
+
+def index():
+    return render_template('index.html')
+
+def add_blog():
+    return render_template('')
+    
