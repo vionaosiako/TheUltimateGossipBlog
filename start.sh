5295112336de10e7970a196c5c28fc9d123e7974
vionaosiako <viona.osiako@student.morigaschool.com>
2022-05-13 14:50:56 +0300

    Add basic app structure

---
+++ start.sh
@@ -0,0 +1,6 @@
+QUOTE_API_KEY='http://quotes.stormconsultancy.co.uk/random.json'
+export SECRET_KEY='vivioonana123'
+MAIL_USERNAME='montezviona@gmail.com'
+MAIL_PASSWORD='silaomuka'
+
+python3 manage.py server
